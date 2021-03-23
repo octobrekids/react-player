cb6a62f7b2e21ffcb5ba293514479ee81fb399a5
octobrekids <chnrd.k@gmail.com>
2021-03-24 02:01:13 +0700

    change the video

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="playerWrapper">
       <Row justify="center">
         <Col span={12}>
-          <ReactPlayer url="https://www.youtube.com/watch?v=AMMx1hOwH-4" 
+          <ReactPlayer url="https://www.youtube.com/watch?v=gdZLi9oWNZg" 
           muted={true}
           playing={true} />
         </Col>
