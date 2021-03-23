45088a981dd5a846f188cde70f6b9ce5a254b800
octobrekids <chnrd.k@gmail.com>
2021-03-24 02:24:47 +0700

    add top controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import './App.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 import ReactPlayer from 'react-player';
+// top: 50%;
+// left: 50%;
+// position: absolute;
+// transform: translate(-50%,-50%);
+// text-align: center;
 
 function App() {
   return (
-    <div className="playerWrapper">
-      <Row justify="center">
-        <Col span={12}>
-          <ReactPlayer url="https://www.youtube.com/watch?v=gdZLi9oWNZg" 
-          muted={true}
-          playing={true} />
-        </Col>
-      </Row>
-    </div>
+    <Row justify="center">
+      <Col span={10}>
+        <div className="playerWrapper">
+          <ReactPlayer url="https://www.youtube.com/watch?v=gdZLi9oWNZg"
+            width="100%"
+            height="100%"
+            muted={true}
+            playing={true} />
+          <div className="controlsWrapper">
+            <Row justify="space-between" style={{padding:16}}>
+              <Col>
+                <h5>Video Title</h5>
+              </Col>
+              <Col>
+              <Button>Bookmark
+                </Button></Col>
+            </Row>
+          </div>
+        </div>
+      </Col>
+    </Row>
   );
 }
 
