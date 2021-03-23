25e3121cdb8046472110008b85e1e1e4c31ba836
octobrekids <chnrd.k@gmail.com>
2021-03-24 02:00:39 +0700

    setting default ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Row, Col } from 'antd';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
-    <div className="App">
- 
+    <div className="playerWrapper">
+      <Row justify="center">
+        <Col span={12}>
+          <ReactPlayer url="https://www.youtube.com/watch?v=AMMx1hOwH-4" 
+          muted={true}
+          playing={true} />
+        </Col>
+      </Row>
     </div>
   );
 }
