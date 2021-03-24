8fd2121d0239c633b21c8878516e3693025a1773
octobrekids <chnrd.k@gmail.com>
2021-03-25 02:04:14 +0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,57 @@
-import React, { useRef } from 'react';
-import './App.css';
-import { Row, Col } from 'antd';
-import ReactPlayer from 'react-player';
-import PlayerControl from './components/PlayerControl/PlayerControl';
-import { useDispatch, useSelector } from 'react-redux';
-import { StoresState } from './stores';
-import { setPlaying } from './stores/videoReducer';
+import React, { useRef } from "react";
+import "./App.css";
+import { Row, Col } from "antd";
+import ReactPlayer from "react-player";
+import PlayerControl from "./components/PlayerControl/PlayerControl";
+import { useDispatch, useSelector } from "react-redux";
+import { StoresState } from "./stores";
+import { setPlaying } from "./stores/videoReducer";
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const playerRef = useRef<ReactPlayer>(null);
 
-  const playing = useSelector((state: StoresState) => state.videoPlayer.playing)
-  const muted = useSelector((state: StoresState) => state.videoPlayer.muted)
-
+  const playing = useSelector(
+    (state: StoresState) => state.videoPlayer.playing
+  );
+  const muted = useSelector((state: StoresState) => state.videoPlayer.muted);
 
   const handlePlaying = () => {
     dispatch(setPlaying());
-  }
+  };
 
   const handleRewind = () => {
     if (playerRef.current) {
       playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
     }
-  }
+  };
 
   const handleFastForward = () => {
     if (playerRef.current) {
       playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10);
     }
-  }
+  };
 
   return (
     <Row justify="center">
       <Col span={10}>
         <div className="playerWrapper">
-          <ReactPlayer url="https://www.youtube.com/watch?v=gdZLi9oWNZg"
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=gdZLi9oWNZg"
             ref={playerRef}
             width="100%"
             height="100%"
             muted={muted}
-            playing={playing} />
+            playing={playing}
+          />
           <PlayerControl
             muted={muted}
             playing={playing}
             handleRewind={handleRewind}
             handleFastForward={handleFastForward}
-            handlePlaying={handlePlaying} />
+            handlePlaying={handlePlaying}
+          />
         </div>
       </Col>
     </Row>
--- src/stores/videoReducer/index.ts
+++ src/stores/videoReducer/index.ts
@@ -2,16 +2,16 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initVideoPlayerState } from "./init";
 
 const videoPlayerSlice = createSlice({
-    name: "videoPlayer",
-    initialState: initVideoPlayerState,
-    reducers: {
-        setMuted(state) {
-            state.muted = !state.muted;
-        },
-        setPlaying(state) {
-            state.playing = !state.playing;
-        }
+  name: "videoPlayer",
+  initialState: initVideoPlayerState,
+  reducers: {
+    setMuted(state) {
+      state.muted = !state.muted;
+    },
+    setPlaying(state) {
+      state.playing = !state.playing;
     }
+  }
 });
 
 export const { setMuted, setPlaying } = videoPlayerSlice.actions;
