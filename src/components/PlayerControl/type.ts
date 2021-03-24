bf1eabaa3a3b30ac48b2d310a1763b57391ed306
octobrekids <chnrd.k@gmail.com>
2021-03-25 01:30:15 +0700

    add function type

--- src/components/PlayerControl/type.ts
+++ src/components/PlayerControl/type.ts
@@ -2,4 +2,6 @@ import { initVideoPlayerStateType } from "../../stores/videoReducer/type";
 
 export type PlayerControlPropsType = Omit<initVideoPlayerStateType, "volume"> & {
     handlePlaying: () => void,
+    handleFastForward: () => void,
+    handleRewind: () => void,
 };
