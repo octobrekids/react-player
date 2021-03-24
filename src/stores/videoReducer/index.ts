75c579c3990d0d59065efcec6d98ea43fa2affa4
octobrekids <chnrd.k@gmail.com>
2021-03-24 20:58:53 +0700

    add actions

--- src/stores/videoReducer/index.ts
+++ src/stores/videoReducer/index.ts
@@ -1,10 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initVideoPlayerState } from "./init";
 
 const videoPlayerSlice = createSlice({
     name: 'videoPlayer',
     initialState: initVideoPlayerState,
-    reducers: {}
+    reducers: {
+        setMuted(state, action: PayloadAction<{muted: boolean}>) {
+            state.muted =  !state.muted;
+        },
+        setPlaying(state, action: PayloadAction<{playing: boolean}>) {
+            state.playing =  !action.payload.playing;
+        },
+    }
 })
 
+export const { setMuted, setPlaying }  = videoPlayerSlice.actions;
 export default videoPlayerSlice.reducer
