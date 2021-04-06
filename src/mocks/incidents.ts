03dcccd4aa00bd1c2cc1417bb915c3cdc9a26010
octobrekids <chnrd.k@gmail.com>
2021-04-07 02:20:12 +0700

    add mock incidents

---
+++ src/mocks/incidents.ts
@@ -0,0 +1,24 @@
+export const incidents = [
+	{
+		x: 100,
+		y: 100,
+		height: 200,
+		width: 200,
+		label: 'dog',
+		color: 'red',
+		visibleDuration: 20 * 1000,
+		visibleCountdown: 0,
+		start: 5 * 1000,
+	},
+	{
+		x: 175,
+		y: 150,
+		height: 300,
+		width: 350,
+		label: 'cat',
+		color: 'blue',
+		visibleDuration: 150 * 1000,
+		visibleCountdown: 0,
+		start: 5 * 1000,
+	},
+];
