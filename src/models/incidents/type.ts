0cee6994a5d6c3b6141ae7de12301f8c3f324cf8
octobrekids <chnrd.k@gmail.com>
2021-04-06 22:35:18 +0700

    add incident type

---
+++ src/models/incidents/type.ts
@@ -0,0 +1,11 @@
+export type incident = {
+	x: number;
+	y: number;
+	height: number;
+	width: number;
+	label: string;
+	color?: string;
+	visibleDuration: number;
+	visibleCountdown: number;
+	startVisibleAt: number;
+};
